7d5e9f2636c3e4b8c6a6c368be4d0bfa7ecf1639
Maxwell BURSON <mburson@e1z2r3p43.42.us.org>
2017-01-07 00:00:14 -0800

    now handles numbers int size or smaller excluding unsinged ints in regular format printing

--- print_arg.c
+++ print_arg.c
@@ -14,41 +14,50 @@
 #include <ft_printf.h>
 #include <libft.h>
 #include <stdlib.h>
+#include <string.h>
 
-/*
-static void		handle_int(int argument, t_conv *conv)
+static char		*handle_int(t_conv *conv, va_list *ap)
 {
+	int		result;
 
+	result = (int)va_arg(*ap, int);
+	if (conv->letter == 'd' || conv->letter == 'i')
+	{
+		if (conv->length == HH_LENGTH)
+			result = (int)(char)result;
+		else if (conv->length == H_LENGTH)
+			result = (int)(short)result;
+	}
+	return (ft_itoa(result));
 }
 
-static void		handle_float(int argument, t_conv *conv)
-{
-	
-}
-
-static void		handle_double(int argument, t_conv *conv)
-{
-	
-}
-
-static void		handle_char(int argument, t_conv *conv)
-{
-	
-}
-*/
 static char		*pre_format(t_conv *conv, va_list *ap)
 {
 	if (conv->letter == 's')
-		return (va_arg(*ap, char*));
-	return (" NO FORMAT FOUND ");
+	{
+		return (ft_strdup(va_arg(*ap, char*)));
+	}
+	else if (conv->length == HH_LENGTH || conv->length == H_LENGTH
+				|| (conv->length == 0 && (ft_strchr("diuoxX", conv->letter))))
+	{
+		return (handle_int(conv, ap));
+	}
+	else
+		return (NULL);
 }
 
 void			print_arg(t_conv *conv, va_list *ap)
 {
 	char *result;
 
-	print_conv(conv);
+	//print_conv(conv);
 
 	result = pre_format(conv, ap);
-	ft_putstr(result);
+	if (result == NULL)
+		ft_putstr("ERROR");
+	else
+	{
+		ft_putstr(result);
+		free(result);
+	}
 }
