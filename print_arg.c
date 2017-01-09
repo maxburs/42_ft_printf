4f035cbe19aa21dc4d098b37d6fec8769bb82070
Maxwell BURSON <mburson@e1z2r3p33.42.us.org>
2017-01-08 19:43:09 -0800

    now handles anything int size or smaller, has bugs though

--- print_arg.c
+++ print_arg.c
@@ -15,14 +15,9 @@
 #include <libft.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-static char		*handle_int(t_conv *conv, va_list *ap)
-{
-	int		result;
-
-	result = (int)va_arg(*ap, int);
-	conv->letter++;
-	/* WHY DOESNT THIS SEEM TO DO ANYTHING KAYNE??
+/* WHY DOESNT THIS SEEM TO DO ANYTHING KAYNE??
 	if (conv->letter == 'd' || conv->letter == 'i')
 	{
 		if (conv->length == HH_LENGTH)
@@ -31,7 +26,40 @@ static char		*handle_int(t_conv *conv, va_list *ap)
 			result = (int)(short)result;
 	}
 	*/
-	return (ft_itoa(result));
+
+static char		*handle_int(t_conv *conv, va_list *ap)
+{
+	unsigned long long int		result;
+	_Bool						neg;
+	char						*str;
+
+	if (conv->length <= 3)
+	{
+		result = (int)va_arg(*ap, int);
+	}
+	
+	if ((conv->letter == 'd' || conv->letter == 'i')
+		&& (signed long long int)result < 0)
+	{
+		neg = true;
+		result = ft_absolute((signed long long int)result);
+	}
+	else
+		neg = false;
+
+	if (conv->letter == 'd' || conv->letter == 'i' || conv->letter == 'u')
+		str = (ft_itoa_base(result, 10, false));
+	else if (conv->letter == 'o')
+		str = (ft_itoa_base(result, 8, false));
+	else if (conv->letter == 'x')
+		str = (ft_itoa_base(result, 16, false));
+	else if (conv->letter == 'X')
+		str = (ft_itoa_base(result, 16, true));
+
+	if (neg)
+		return (ft_strjoin("-", str)); //TODO: FIX THIS! MEMORY LEAK HERE
+	else
+		return (str);
 }
 
 static char		*pre_format(t_conv *conv, va_list *ap)
@@ -40,8 +68,7 @@ static char		*pre_format(t_conv *conv, va_list *ap)
 	{
 		return (ft_strdup(va_arg(*ap, char*)));
 	}
-	else if (conv->length == HH_LENGTH || conv->length == H_LENGTH
-				|| (conv->length == 0 && (ft_strchr("diuoxX", conv->letter))))
+	else if (ft_strchr("diuoxX", conv->letter))
 	{
 		return (handle_int(conv, ap));
 	}
